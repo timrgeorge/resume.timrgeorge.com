a02bf7eccbcd881926848438e3c1d7bb1852ed34
Tim George <tim@timrgeorge.com>
2015-03-05 14:08:13 -0500

    Resume updates

--- index.php
+++ index.php
@@ -6,39 +6,39 @@
 <!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="en"> <![endif]-->
 <!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
 
-<!-- ************************************************************** -->
-<!-- Hey, thanks for checking out my source code and seeing what    -->
-<!-- I'm all about. If you like what you see, please feel free to   -->
-<!-- get in touch with me at: tim@timrgeorge.com                    -->
-<!--                                                                -->
-<!-- Thanks again for coming by!                                    -->
-<!-- For your efforts, please enjoy the following:                  -->
-<!-- https://www.youtube.com/watch?v=AZKpByV5764                    -->
-<!-- or                                                             -->
-<!-- https://www.youtube.com/watch?v=3Sso3h0xMJA                    -->
-<!-- ************************************************************** -->
+<!-- ************************************************************************ -->
+<!--                                                                          -->
+<!-- Hey, thanks for checking out my source code and seeing what              -->
+<!-- I'm all about. If you like what you see, please feel free to             -->
+<!-- get in touch with me at: tim@timrgeorge.com                              -->
+<!--                                                                          -->
+<!-- Thanks again for coming by!                                              -->
+<!-- For your efforts, please enjoy the following:                            -->
+<!-- https://www.youtube.com/watch?v=AZKpByV5764                              -->
+<!-- or                                                                       -->
+<!-- https://www.youtube.com/watch?v=3Sso3h0xMJA                              -->
+<!--                                                                          -->
+<!-- Orbitron Font:       https://www.theleagueofmoveabletype.com/orbitron    -->
+<!-- Initial Templates:   https://github.com/murtaugh/HTML5-Reset             -->
+<!--                                                                          -->
+<!-- ************************************************************************ -->
 
 <head>
-
 <meta charset="utf-8">
-
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 <title>Resume | Tim George</title>
 <meta name="author" content="Tim George" />
-<meta name="description" content="Tim often enjoys coding to Stan Bush's 'The Touch'." />
-
-<meta name="google-site-verification" content="jjAew9bmyHsT_U5BIFoSsjSd01R96zaSarkJjZJB894" />
-
+<meta name="description" content="Resume of Tim George - UI/UX &amp; Web Developer." />
 <meta name="Copyright" content="<?php echo date('Y')?> - Tim George" />
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="jjAew9bmyHsT_U5BIFoSsjSd01R96zaSarkJjZJB894" />
 
 <link rel="shortcut icon" href="assets/img/favicon.ico" />
 <link rel="stylesheet" href="assets/css/style.css" />
 
 <script src="assets/js/libs/modernizr-2.8.3.js"></script>
-
 </head>
 
 <body>
@@ -50,8 +50,8 @@
 </header>
 
 <section class="content objective">
-<h2>Objective</h2>    
-<p>To obtain a career opportunity in an Information Technology setting in the areas of Web Development and Digital Media</p>  
+<h2>Objective</h2>
+<p>To obtain career opportunity in a dynamic Information Technology setting with emphasis in the areas of Web Development and Digital Media.</p>  
 </section>
 
 <section class="content education">
@@ -64,7 +64,7 @@
 <h2>Skills</h2>
 <p><strong>Markup / Programming Languages &amp; Libraries</strong><br />HTML5/(X)HTML, XML, CSS(2/3), JavaScript, jQuery, Backbone.js, Spring MVC, Apache Velocity, Apache Tiles, PHP, ASP, ASP.NET, VB, MySQL, MS SQL, Oracle DBMS, MS SQL, Java, C++, C#</p>
 <p><strong>Blog / CMS / Platforms:</strong><br />Perch, WordPress, Ektron CMS 400, SharePoint</p>
-<p><strong>Version Control:</strong><br />GIT, CVS, SVN, TFS, VSS</p>
+<p><strong>Version Control:</strong><br />GIT, TFS, VSS</p>
 <p><strong>Operating Systems:</strong><br />Windows (8/7/Vista/XP/9X), Mac OSX, Linux, DOS</p>
 <p><strong>Software Packages:</strong><br />Sublime Text 2, Spring Tool Suite, Eclipse, Microsoft Office (Word, Excel, Access, PowerPoint, FrontPage, Publisher, Visio, Visual Studio), Adobe Creative Suite (Flash, Fireworks, DreamWeaver, Photoshop, Illustrator), Adobe Director, Pro Tools 7LE</p>
 <p class="no-padding"><strong>Other:</strong></p>
@@ -81,47 +81,71 @@
 <h2>Experience</h2>
 
 <p>
-<em>Managing UI/UEX Architect - E-Business Consulting</em>
 <strong>Boston Financial Data Services</strong>
-<small>Quincy, MA  -  January 2014 to Present</small>
+<small>Quincy, MA  -  October 2009 to Present</small>
+</p>
+
+<p class="no-padding">
+<em>Managing UI/UEX Architect; E-Business Consulting</em>
+<small>December 2013 to Present</small>
 </p>
 <ul>
 <li>Responsible for consulting with internal/external clients and proposing user experience solutions</li>
 <li>Lead a team of UI/UEX consultants that played a critical role in the planning of site content and functionality</li>
 <li>Collaborated with the development &amp; support teams to ensure proper translation of the overall strategy/vision</li>
-<li>Continued role of Lead Technical Consultant, providing guidance to development team</li>
+<li>Continued role of Lead Technical Consultant, providing guidance/mentoring to development team</li>
 </ul>
 
-<p>
-<em>Lead Technical Consultant / Web &amp; UI Developer - E-Business Consulting</em>
-<strong>Boston Financial Data Services</strong>
-<small>Quincy, MA  -  September 2009 to December 2013</small>
+<p class="job-title">
+<em>Lead Technical Consultant / Web &amp; UI Developer; E-Business Consulting</em>
+<small>March 2012 to December 2013</small>
 </p>
 <ul>
 <li>Worked within new E-Business initiative group to provide clients with new services and opportunities</li>
 <li>Created new Secure Account Access site while working with a middleware development team</li>
-<li>Developed mobile sites from the ground up, using responsive web design practices, while focusing on content first</li>
+<li>Developed mobile sites, using responsive web design practices, while focusing on content first</li>
 <li>Used progressive enhancement to serve HTML5 and CSS3 elements, and still be compatible in older browsers</li>
+</ul>
+
+<p class="job-title">
+<em>Web Developer - Contract Employment through Kelly Services</em>
+<small>September 2011 to March 2012</small>
+</p>
+<ul>
+<li>Worked remotely from Pennsylvania as a Web Developer Contractor</li>
+<li>Developed a public mobile site for client utilizing HTML5 / CSS Media Queries</li>
 <li>Administered updates to Intranet, Extranet, and Internet sites</li>
 </ul>
 
-<p>
+
+<p class="job-title">
 <em>Web Developer</em>
-<strong>True Action, an eBay Enterprise Company</strong>
-<small>King of Prussia, PA  -  September 2011 to February 2012</small>
+<small>October 2009 to September 2011</small>
 </p>
 <ul>
-<li>Developed cross-compatible HTML email templates for multiple clients</li>
-<li>Maintained and developed new HTML5 functionality to replace older Flash content</li>
-<li>Gained knowledge of working with and updating code across multiple repositories</li>
-<li>Gained additional knowledge of working in technology for a marketing agency</li>
+<li>Maintained and updated templates for ten different (but similar) sites of one of our clients</li>
+<li>Modified templates existing code to meet current web standards</li>
+<li>Learned the importance of managing time across multiple facets of the same project</li>
 </ul>
 
 <p>
-<em>Web Developer</em>
-<strong>Aramark (WearGuard)</strong>
+<strong>True Action, an eBay Enterprise Company</strong>
+<small>King of Prussia, PA  -  September 2011 to February 2012</small>
+</p>
+<p class="job-title"><em>Web Developer</em></p>
+<ul>
+<li>Developed cross-compatible HTML email templates for multiple clients</li>
+<li>Developed interactive solutions that were HTML5 compatible to work on iPads</li>
+<li>Gained knowledge of working with and updating code across multiple repositories</li>
+<li>Learned the importance of a QA and Release Management system to identify/prevent issues</li>
+<li>Cultivated important relationships within all development teams to ensure a common goal</li>
+</ul>
+
+<p>
+<strong>Aramark Uniform Services (WearGuard)</strong>
 <small>Norwell, MA  -  October 2008 to October 2009</small>
 </p>
+<p class="job-title"><em>Web Developer</em></p>
 <ul>
 <li>Revamped existing template files in an effort to make them cross-browser compatible</li>
 <li>Designed and developed a blogging platform for corporate use</li>
@@ -129,25 +153,15 @@
 <li>Gained additional knowledge of administering an ecommerce website</li>
 </ul>
 
-<p>
-<em>Web Technologies Contractor</em>
-<strong>Kelly Services - Boston Financial Data Services</strong>
-<small>North Quincy, MA  -  June 2008 to September 2008</small>
-</p>
-<ul>
-<li>Administered updates to Intranet, Extranet, and Internet sites</li>
-<li>Created inventory databases as well as client web applications</li>
-<li>Participated in many teamwork strategies and gained significant experience in a corporate setting</li>
-</ul>
 
 <p id="work-toggle"><a href="" title="View Additional Work Experience">View Additional Work Experience &raquo;</a></p>
 
 <div id="additional-work">
 <p>
-<em>Web Technologies Internship</em>
 <strong>Kelly Services - Boston Financial Data Services</strong>
-<small>North Quincy, MA  -  June to August 2007</small>
+<small>North Quincy, MA  -  June 2008 to September 2008</small>
 </p>
+<p class="job-title"><em>Web Technologies Contractor</em></p>
 <ul>
 <li>Administered updates to Intranet, Extranet, and Internet sites</li>
 <li>Created inventory databases as well as client web applications</li>
@@ -155,10 +169,21 @@
 </ul>
 
 <p>
-<em>Summer Intern for Business Management Unit</em>
+<strong>Kelly Services - Boston Financial Data Services</strong>
+<small>North Quincy, MA  -  June to August 2007</small>
+</p>
+<p class="job-title"><em>Web Technologies Internship</em></p>
+<ul>
+<li>Administered updates to Intranet, Extranet, and Internet sites</li>
+<li>Created inventory databases as well as client web applications</li>
+<li>Participated in many teamwork strategies and gained significant experience in a corporate setting</li>
+</ul>
+
+<p>
 <strong>The Gillette Company</strong>
 <small>Boston, MA  -  June to August 2006</small>
 </p>
+<p class="job-title"><em>Summer Intern for Business Management Unit</em></p>
 <ul>
 <li>Assisted with the integration of Gillette into the Procter &amp; Gamble company</li>
 <li>Utilized existing software to develop an easy-to-use application for database comparisons</li>
@@ -166,10 +191,10 @@
 </ul>
 
 <p>
-<em>Web Technologies Internship</em>
 <strong>Adecco - Boston Financial Data Services</strong>
 <small>Braintree, MA  -  June to August 2005</small>
 </p>
+<p class="job-title"><em>Web Technologies Internship</em></p>
 <ul>
 <li>Administered updates to Intranet, Extranet, and Internet sites</li>
 <li>Created inventory databases as well as client web applications</li>
@@ -215,7 +240,6 @@
 
   ga('create', 'UA-709645-11', 'auto');
   ga('send', 'pageview');
-
 </script>
 
 </body>
